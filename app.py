378761231d2949d57e417814e41df643187c4a3a
Claromes <claromes@hey.com>
2023-09-10 12:13:14 -0300

    add columns, selectbox for lang options

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import easyocr
-from PIL import Image
 import io
 import tempfile
 import os
+
+from PIL import Image
 from googletrans import Translator
 
-FROM_LANG = 'ru'
+st.title('extt', anchor=False)
+st.caption('tool for extracting and translating text from image based in [EasyOCR](https://github.com/JaidedAI/EasyOCR) and a [implementation of Google Translate API](https://github.com/ssut/py-googletrans)')
 
-def translate(text, from_lang=FROM_LANG, to_lang='en'):
-    translator = Translator()
-    translate = translator.translate(text, src=from_lang, dest=to_lang)
-    return translate.text
+uploaded_files = st.file_uploader('upload files', help='we do not store your files', accept_multiple_files=True)
 
-st.title('extt')
+col1, col2 = st.columns(2)
 
-uploaded_files = st.file_uploader('upload a picture', help='we do not store your file', accept_multiple_files=True)
-st.empty()
-button = st.button('extract and translate', type='primary')
+with col1:
+    from_lang = st.selectbox(
+        'from lang',
+        ('en', 'ru', 'pt', 'es', 'fr'))
 
-temp_dir = tempfile.TemporaryDirectory()
+with col2:
+    to_lang = st.selectbox(
+        'to lang',
+        ('en', 'ru', 'pt', 'es', 'fr'))
+
+_, colb, _ = st.columns(3)
+with colb:
+    button = st.button('extract and translate', type='primary')
+
+st.divider()
 
 if button:
-    reader = easyocr.Reader([FROM_LANG])
+
+    def translate(text, from_lang=from_lang, to_lang=to_lang):
+        translator = Translator()
+        translate = translator.translate((text), src=from_lang, dest=to_lang)
+        return translate.text
+
+    temp_dir = tempfile.TemporaryDirectory()
+
+    reader = easyocr.Reader([from_lang])
 
     for uploaded_file in uploaded_files:
         bytes_data = uploaded_file.read()
@@ -34,7 +52,21 @@ if button:
 
         result = reader.readtext(temp_image_path)
 
-        for (_, coord, _) in result:
-            st.write(coord)
-            st.write(f'translation: {translate(coord)}')
-            st.divider()
+        colimg, coltext = st.columns(2)
+
+        with colimg:
+            st.image(temp_image_path)
+
+        with coltext:
+            st.subheader(f'{uploaded_file.name}', anchor=False, divider='rainbow')
+            for (_, coord, _) in result:
+                try:
+                    st.write(f'''
+                    {coord}
+
+                    **translation:** {translate(coord)}
+                    ''')
+                    st.divider()
+                except TypeError as e:
+                    st.error(f'error: Unable to translate "{coord}"')
+                    st.divider()
